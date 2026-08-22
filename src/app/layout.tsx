97c748e302e0b76404ae46acf731597f2cc0a8aa
kimhyunjoo <khjoo1203@gmail.com>
2026-08-22 20:44:39 +0900

    feat: 폰트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+
+const baloo = localFont({
+  src: "../../public/fonts/Baloo2-ExtraBold.ttf",
+  variable: "--font-baloo",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -19,11 +17,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="ko" className={baloo.variable}>
+      <body>{children}</body>
     </html>
   );
 }
