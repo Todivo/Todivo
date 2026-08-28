bc303baa88c2810162e787867cb229c33ee8d3e3
kimhyunjoo <khjoo1203@gmail.com>
2026-08-28 16:57:46 +0900

    chore: tailwindcss 프리티어

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -6,9 +6,9 @@ module.exports = {
   proseWrap: 'preserve', //Markdown 문장의 줄바꿈 유지
   quoteProps: 'as-needed', //필요한 경우에만 객체 키를 따옴표로 감쌈
   semi: true, //문장 끝에 세미콜론 사용
-  singleQuote: true, //문자열에 작은따옴표 사용
+  singleQuote: false, //문자열에 큰따옴표 사용
   tabWidth: 2, //들여쓰기 크기
   trailingComma: 'all', //가능한 모든 곳에 후행 쉼표(trailing comma)를 붙이는 설정
   plugins: ['prettier-plugin-tailwindcss'], // 프리티어 자동정렬
-  useTabs: false, //Tab 대신 Space 사용
+  tailwindStylesheet: './src/app/globals.css', // Tailwind v4 CSS 진입점 (클래스 정렬용)
 };
